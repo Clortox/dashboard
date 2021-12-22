5d2a3c8f946ce93d59d9c0ef171e6447a41f3fb9
Tyler Perkins <tperki17@kent.edu>
2021-12-21 19:42:26 -0500

    Add contract to qrcode

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -6,7 +6,7 @@ if [ $# -ne 2 ]; then
     echo "The result will be in wifi.png"
 fi
 
-qrencode -s 6 --background=FFFFFF00 -o "./img/wifi.png" "WIFI:T:WPA;S:$1;P:$2;;"
+qrencode -s 6 --background=D3D3D37F -o "./img/wifi.png" "WIFI:T:WPA;S:$1;P:$2;;"
 
 echo "Your wifi qrcode is wifi.png !"
 echo "Your wifi qrcode is now in the local img folder as wifi.png"
